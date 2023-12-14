174f72894331a536056b62be719ae23e60adbda3
Orfan Al Malki <orfan.al.malki@accenture.com>
2023-12-14 10:54:25 +0100

    task tracker page and style

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { FaTasks } from "react-icons/fa";
+import classnames from "classnames";
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaTasks />
+      </Link>
+
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            // className={`${
+            //   link.href === currentPath ? "text-zinc-900" : " text-zinc-500"
+            // } hover:text-zinc-950 transition-colors`}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-950 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
