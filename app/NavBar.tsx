ec4f0109b76269c30e8c4cba6bae87729b89d9ec
Orfan Al Malki <orfan.al.malki@accenture.com>
2023-12-14 11:54:41 +0100

    Api for creating task

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Tasks", href: "/tasks" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
